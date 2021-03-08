5e822b20ab157d1692cb4a4e115e9c9cf7bce511
Indigo-w <862891330@qq.com>
2021-03-08 17:01:27 +0800

    新建登陆组件配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '../components/login/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      name: 'login',
+      path: '/login',
+      component: Login
     }
-  ]
+  ],
+  mode: "history"
 })
